00c71c635bec899efa800a63a87e708102159ae0
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-08 17:29:17 +0000

    Updated checksums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mantis_connect/trunk@36513 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mantis_connect".
 #
-# Auto generated 03-08-2010 09:04
+# Auto generated 08-08-2010 19:28
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -42,6 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"c21a";s:16:"ext_autoload.php";s:4:"0f5e";s:12:"ext_icon.gif";s:4:"b640";s:17:"ext_localconf.php";s:4:"929b";s:14:"ext_tables.php";s:4:"8632";s:16:"locallang_db.xml";s:4:"1fdd";s:47:"connectors/class.tx_mantisconnect_powermail.php";s:4:"6eac";s:51:"connectors/interface.tx_mantisconnect_connector.php";s:4:"7301";s:14:"doc/manual.sxw";s:4:"fcf8";s:37:"lib/class.tx_mantisconnect_mantis.php";s:4:"2212";s:36:"lib/user_mantisconnect_powermail.php";s:4:"6fc1";s:39:"pi1/class.tx_mantisconnect_flexform.php";s:4:"b2d7";s:34:"pi1/class.tx_mantisconnect_pi1.php";s:4:"b1be";s:16:"pi1/flexform.xml";s:4:"8726";s:17:"pi1/locallang.xml";s:4:"a0c2";s:20:"static/constants.txt";s:4:"793f";s:16:"static/setup.txt";s:4:"625c";}',
 );
 
 ?>
